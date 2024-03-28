ae9a9babe8f7af4ecf7605f1129eda13fc58a972
Apsiingh <a.p.singh5508@gmail.com>
2024-03-28 10:35:29 +0530

    wanderla

---
+++ app.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override"); // Corrected typo
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+
+// DB CONNECTION START
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+// DB CONNECTION END
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); // Corrected typo
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.send("Welcome To Root");
+});
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.validate(req.body).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//Index Route
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+// New Route
+// IMPORTANT :- NEW ALWAYS BE UPPER THAN SHOW
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new");
+});
+
+// Show Route
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+// Create Route
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    // creating instance
+    const newListing = new Listing(req.body.listing);
+    await newListing.save(); // Added await here
+    res.redirect("/listings");
+  })
+);
+
+// Edit Route
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+// Update Route
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+// Delete Route
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id); // Added await here
+    res.redirect("/listings");
+  })
+);
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
+});
+
+// MIDDLEWARE FOR ERROR HANDLING
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "SomeThing Went Wrong" } = err;
+  res.status(statusCode).render("error.ejs", { err });
+});
+
+let port = 8080;
+app.listen(port, () => {
+  console.log("server is running on the port :", port);
+});
---
+++ schema.js
@@ -0,0 +1,15 @@
+const joi = require("joi");
+const { model } = require("mongoose");
+
+module.exports.listingSchema = joi.object({
+  listing: joi
+    .object({
+      title: joi.string().required(),
+      description: joi.string().required(),
+      price: joi.number().required().min(0),
+      country: joi.string().required(),
+      location: joi.string().required(),
+      image: joi.string().allow("",null)
+    })
+    .required(),
+});
