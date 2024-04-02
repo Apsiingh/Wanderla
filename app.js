dbff6801da99afc4cb10600d919cb5178383ae10
Apsiingh <a.p.singh5508@gmail.com>
2024-04-03 02:11:34 +0530

    session for cookie

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const path = require("path");
 const methodOverride = require("method-override"); // Corrected typo
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
 
 const listings = require("./routers/listing.js");
-const review  = require("./routers/review.js")
+const review = require("./routers/review.js");
 
 // DB CONNECTION START
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -30,16 +31,25 @@ app.use(methodOverride("_method")); // Corrected typo
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
+  httpOnly: true,
+};
+
+app.use(session(sessionOptions));
+
 app.get("/", (req, res) => {
   res.send("Welcome To Root");
 });
 
-
-
-
 app.use("/listings", listings);
-app.use("/listings/:id/reviews", review)
-
+app.use("/listings/:id/reviews", review);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
