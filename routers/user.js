3a7b03feaaa2c73ecde14196cfc5524625dba46a
Apsiingh <a.p.singh5508@gmail.com>
2024-04-10 10:27:28 +0530

    saving redirectUrl were User can redirect directly to the requested url after login

--- routers/user.js
+++ routers/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -22,7 +23,7 @@ router.post(
           return next(err);
         } else {
           req.flash("success", "Welcome To WanderLa");
-          res.redirect("/listings");
+          res.redirect(req.session.redirectUrl);
         }
       });
     } catch (error) {
@@ -38,13 +39,20 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
     req.flash("success", "Welcome Back to Wanderla");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+    // if(redirectUrl){
+    //   res.redirect(res.locals.redirectUrl);
+    // }else{
+    //   res.redirect("/listings")
+    // }
   }
 );
 
