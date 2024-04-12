faa4bda5523d40245281b1eeb3e00b5ca44c04ac
Apsiingh <a.p.singh5508@gmail.com>
2024-04-13 02:14:42 +0530

    controllers for user

---
+++ controllers/users.js
@@ -0,0 +1,51 @@
+const User = require("../models/user.js");
+
+module.exports.rendersignupForm = (req, res) => {
+  res.render("users/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    console.log(username, email, password);
+    const newUser = new User({ email, username });
+    const registerUser = await User.register(newUser, password);
+    console.log(registerUser);
+    req.login(registerUser, (err) => {
+      if (err) {
+        return next(err);
+      } else {
+        req.flash("success", "Welcome To WanderLa");
+        res.redirect("/listings");
+      }
+    });
+  } catch (error) {
+    req.flash("error", error.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.login = async (req, res) => {
+  req.flash("success", "Welcome Back to Wanderla");
+  let redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+  // if(redirectUrl){
+  //   res.redirect(res.locals.redirectUrl);
+  // }else{
+  //   res.redirect("/listings")
+  // }
+};
+
+module.exports.logout = (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "you are logged out!");
+    res.redirect("/listings");
+  });
+};
--- routers/user.js
+++ routers/user.js
@@ -1,41 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+// controller
+const userController = require("../controllers/users.js");
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      console.log(username, email, password);
-      const newUser = new User({ email, username });
-      const registerUser = await User.register(newUser, password);
-      console.log(registerUser);
-      req.login(registerUser, (err) => {
-        if (err) {
-          return next(err);
-        } else {
-          req.flash("success", "Welcome To WanderLa");
-          res.redirect("/listings");
-        }
-      });
-    } catch (error) {
-      req.flash("error", error.message);
-      res.redirect("/signup");
-    }
-  })
-);
+router.get("/signup", userController.rendersignupForm);
 
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
+router.post("/signup", wrapAsync(userController.signup));
+
+router.get("/login", userController.renderLoginForm);
 
 router.post(
   "/login",
@@ -44,26 +20,9 @@ router.post(
     failureRedirect: "/login",
     failureFlash: true,
   }),
-  async (req, res) => {
-    req.flash("success", "Welcome Back to Wanderla");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-    // if(redirectUrl){
-    //   res.redirect(res.locals.redirectUrl);
-    // }else{
-    //   res.redirect("/listings")
-    // }
-  }
+  userController.login
 );
 
-router.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "you are logged out!");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
